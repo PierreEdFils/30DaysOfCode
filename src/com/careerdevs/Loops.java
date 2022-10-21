4b8eae084ac0a4c7747381cc83f458827eefb6e3
PierreEdFils <epierrefils@gmail.com>
2022-10-20 20:12:00 -0400

    Day 3, 4, 5 fo 30Days of Code Challenge

---
+++ src/com/careerdevs/Loops.java
@@ -0,0 +1,14 @@
+package com.careerdevs;
+
+public class Loops {
+    public static void main(String[] args) {
+        int n = 0;
+        int i= 1;
+        while (i<11)
+        {
+            System.out.println (n+" x "+i+" = "+(n*i));
+            i++;
+        }
+
+    }
+}
